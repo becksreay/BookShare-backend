d106ce874fda82ce40168d57a65497fabf09cb83
Rebekah Reay <becksreay@gmail.com>
2025-08-08 08:21:56 +0100

    added a landing page

--- book_share/urls.py
+++ book_share/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import landing_page
 
 urlpatterns = [
+    path('', landing_page, name='landing'),
     path('admin/', admin.site.urls),
     path('members/', include('members.urls')),
     # path('books/', include('books.urls')),
---
+++ book_share/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def landing_page(request):
+    return render(request, "landing.html")
+   
